150e04782d3099d9a6cb913030fc05059f9d97f0
AZDemonEdge <rene001208@gmail.com>
2025-07-30 15:56:55 -0400

    set envoirements variables

--- src/Component/Wishlist/db.js
+++ src/Component/Wishlist/db.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdIr-TCQJcV7E4AdeYTJgoSZsEvu2BZj8",
-  authDomain: "my-wishlist-44734.firebaseapp.com",
-  databaseURL: "https://my-wishlist-44734-default-rtdb.firebaseio.com",
-  projectId: "my-wishlist-44734",
-  storageBucket: "my-wishlist-44734.firebasestorage.app",
-  messagingSenderId: "810601711877",
-  appId: "1:810601711877:web:611ac14c46b817fa400f6c",
-  measurementId: "G-9V3TD9XX4S"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
