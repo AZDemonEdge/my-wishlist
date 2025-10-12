0dc63e90ae5d8694ed1ba803a57bbf75ab374f01
AZDemonEdge <rene001208@gmail.com>
2025-10-12 03:40:41 -0400

    Make Visual Corrections at AdminPanel

--- src/Component/AdminPanel/AdminPanel.js
+++ src/Component/AdminPanel/AdminPanel.js
@@ -394,7 +394,21 @@ const AdminPanel = () => {
                                     <td>{wish.id}</td>
                                     <td style={{textAlign: 'center'}}>{wish.Number}</td>
                                     <td>{wish.Description}</td>
-                                    <td style={{textAlign: 'center'}}>{wish.State}</td>
+                                    <td style={{textAlign: 'center'}}>{wish.State===1 ? (
+                                        <span style={{ backgroundColor: 'lightgreen', color: 'darkgreen', padding: 0.3 + 'rem'}}>
+                                            <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M16.0303 10.0303C16.3232 9.73744 16.3232 9.26256 16.0303 8.96967C15.7374 8.67678 15.2626 8.67678 14.9697 8.96967L10.5 13.4393L9.03033 11.9697C8.73744 11.6768 8.26256 11.6768 7.96967 11.9697C7.67678 12.2626 7.67678 12.7374 7.96967 13.0303L9.96967 15.0303C10.2626 15.3232 10.7374 15.3232 11.0303 15.0303L16.0303 10.0303Z" fill="currentColor"/>
+                                                <path fillRule="evenodd" clipRule="evenodd" d="M12 1.25C6.06294 1.25 1.25 6.06294 1.25 12C1.25 17.9371 6.06294 22.75 12 22.75C17.9371 22.75 22.75 17.9371 22.75 12C22.75 6.06294 17.9371 1.25 12 1.25ZM2.75 12C2.75 6.89137 6.89137 2.75 12 2.75C17.1086 2.75 21.25 6.89137 21.25 12C21.25 17.1086 17.1086 21.25 12 21.25C6.89137 21.25 2.75 17.1086 2.75 12Z" fill="currentColor"/>
+                                            </svg> Completado
+                                        </span>
+                                    ) : (
+                                        <span style={{ backgroundColor: 'lightyellow', color: 'orage', padding: 0.3 + 'rem'}}>
+                                            <svg width="20px" height="20px" viewBox="0 0 1024 1024" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M511.9 183c-181.8 0-329.1 147.4-329.1 329.1s147.4 329.1 329.1 329.1c181.8 0 329.1-147.4 329.1-329.1S693.6 183 511.9 183z m0 585.2c-141.2 0-256-114.8-256-256s114.8-256 256-256 256 114.8 256 256-114.9 256-256 256z" fill="currentColor" />
+                                                <path d="M548.6 365.7h-73.2v161.4l120.5 120.5 51.7-51.7-99-99z" fill="currentColor" />
+                                            </svg> Pendiente
+                                        </span>
+                                    )}</td>
                                     <td>
                                         <a className="btn btn-warning" onClick={(e) => {
                                             setCurrentWish({docId: wish.id, Id: wish.Number, State: wish.State, Description: wish.Description, Photo: `./pictures/wish${wish.Number}.png` });
